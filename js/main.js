263f7612cf2db1683c58dab92175a495beb21ba5
Ricardo <me@ricardosa.nz>
2021-10-05 23:24:06 +0200

    Fix navigation not working on mobile

--- js/main.js
+++ js/main.js
@@ -3,19 +3,29 @@ layout: null
 ---
 $(document).ready(function () {
   $('a.blog-button').click(function (e) {
-    $('.about-wrapper').removeClass('animated slideInLeft')
-    $('.content-wrapper').addClass('animated slideInRight')
-    $('.panel-cover').removeClass('panel-cover--collapsed-right')
-    $('.panel-cover').animate({'right': '62%', 'width': '40%'}, 400, swing = 'swing', function () {})
-    $('.panel-cover').addClass('panel-cover--collapsed-left')
+        currentWidth = $(window).width()
+    if (currentWidth < 960) {
+      $('.panel-cover').addClass('panel-cover--collapsed')
+    } else {
+      $('.about-wrapper').removeClass('animated slideInLeft')
+      $('.content-wrapper').addClass('animated slideInRight')
+      $('.panel-cover').removeClass('panel-cover--collapsed-right')
+      $('.panel-cover').animate({'right': '62%', 'width': '40%'}, 400, swing = 'swing', function () {})
+      $('.panel-cover').addClass('panel-cover--collapsed-left')
+    }
   })
 
   $('a.about-button').click(function (e) {
-    $('.content-wrapper').removeClass('animated slideInRight')
-    $('.about-wrapper').addClass('animated slideInLeft')
-    $('.panel-cover').removeClass('panel-cover--collapsed-left')
-    $('.panel-cover').animate({'right': '0', 'width': '67%'}, 400, swing = 'swing', function () {})
-    $('.panel-cover').addClass('panel-cover--collapsed-right')
+    currentWidth = $(window).width()
+    if (currentWidth < 960) {
+      $('.panel-cover').addClass('panel-cover--collapsed')
+    } else {
+      $('.content-wrapper').removeClass('animated slideInRight')
+      $('.about-wrapper').addClass('animated slideInLeft')
+      $('.panel-cover').removeClass('panel-cover--collapsed-left')
+      $('.panel-cover').animate({'right': '0', 'width': '67%'}, 400, swing = 'swing', function () {})
+      $('.panel-cover').addClass('panel-cover--collapsed-right')
+    }
   })
 
   if (window.location.hash && window.location.hash == '#blog') {
